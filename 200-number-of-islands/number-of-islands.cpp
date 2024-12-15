cdc98ddabd98c469762c3310d26961b16b28b237
Amritesh Anand <amriteshanand7@gmail.com>
2024-12-15 13:34:19 -0500

    Time: 19 ms (97.26%) | Memory: 16.2 MB (66.85%) - LeetSync

---
+++ 200-number-of-islands/number-of-islands.cpp
@@ -0,0 +1,36 @@
+class Solution {
+    
+    private:
+    
+    void mark_current_islands(vector<vector<char>> &grid, int i , int j, int rows, int columns){
+        
+        if(i<0 || i>=rows || j<0 || j>=columns || grid[i][j]!='1') return;
+        grid[i][j]='2';
+        
+        mark_current_islands(grid,i+1,j,rows,columns);
+        mark_current_islands(grid,i-1,j,rows,columns);
+        mark_current_islands(grid,i,j+1,rows,columns);
+        mark_current_islands(grid,i,j-1,rows,columns);
+    }
+    
+    
+    
+    public:
+    int numIslands(vector<vector<char>>& grid) {
+        int rows = grid.size();
+        if(rows == 0) return 0;
+        
+        int columns = grid[0].size();
+        int no_of_islands = 0;
+        
+        for(int i = 0; i < rows; i++){
+            for(int j = 0; j <columns; j++){
+                if(grid[i][j]=='1'){
+                    mark_current_islands(grid,i,j,rows,columns);
+                    no_of_islands +=1;
+                }
+            }
+        }
+        return no_of_islands;
+    }
+};
