694b19fed8ac717b21a07922ae42b645920e76ce
Amritesh Anand <amriteshanand7@gmail.com>
2025-01-12 14:52:33 -0800

    Time: 0 ms (100.00%) | Memory: 23.9 MB (17.26%) - LeetSync

--- 543-diameter-of-binary-tree/diameter-of-binary-tree.cpp
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.cpp
@@ -11,30 +11,17 @@
  */
 class Solution {
 public:
-int diameterOfBinaryTree(TreeNode* root) {
-        int d=0;
-        rec(root, d);
+    int diameterOfBinaryTree(TreeNode* root) {
+        int d = 0;
+        rec(root,d);
         return d;
     }
-    
-    int rec(TreeNode* root, int &d) {
-        if(root == NULL) return 0;
-        int ld = rec(root->left, d);
-        int rd = rec(root->right, d);
-        d=max(d,ld+rd);
+
+    int rec(TreeNode* root, int &d){
+        if(root==NULL) return 0;
+        int ld = rec(root->left,d);
+        int rd = rec(root->right,d);
+        d = max(d,ld+rd);
         return max(ld,rd)+1;
     }
-    // int diameterOfBinaryTree(TreeNode* root) {
-    //     int d = 0;
-    //     rec(root,d);
-    //     return d;
-    // }
-
-    // int rec(TreeNode* root, int &d){
-    //     if(root==NULL) return 0;
-    //     int ld = rec(root->left,d);
-    //     int rd = rec(root->left,d);
-    //     d = max(d,ld+rd);
-    //     return max(ld,rd)+1;
-    // }
 };
