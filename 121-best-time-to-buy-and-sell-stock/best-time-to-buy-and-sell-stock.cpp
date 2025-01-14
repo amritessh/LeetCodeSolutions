94475bd887921a3d521e7d11f3c8301035f39782
Amritesh Anand <amriteshanand7@gmail.com>
2025-01-14 10:35:21 -0800

    Time: 5 ms (9.51%) | Memory: 97.4 MB (50.73%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -7,15 +7,32 @@ public:
         int profit = 0;
         int maxProfit = 0;
         while(right<n){
-            if(prices[left] < prices[right]){
-            profit = prices[right]- prices[left];
-            maxProfit = max(maxProfit,profit);
-        }
-        else left = right;
+            if(prices[left]<prices[right]){
+                profit = prices[right] - prices[left];
+                maxProfit = max(maxProfit,profit);
+            }
+            else left = right;
+            right +=1;
 
-        right = right+1;
         }
         return maxProfit;
+
+
+        // int left = 0;
+        // int right = 1;
+        // int n = prices.size();
+        // int profit = 0;
+        // int maxProfit = 0;
+        // while(right<n){
+        //     if(prices[left] < prices[right]){
+        //     profit = prices[right]- prices[left];
+        //     maxProfit = max(maxProfit,profit);
+        // }
+        // else left = right;
+
+        // right = right+1;
+        // }
+        // return maxProfit;
         
     }
 };
