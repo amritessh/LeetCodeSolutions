294f5becf31dc610b673f66a31f2d2c42af87028
Amritesh Anand <amriteshanand7@gmail.com>
2025-02-12 13:20:50 -0800

    Time: 0 ms (100.00%) | Memory: 8.4 MB (38.86%) - LeetSync

--- 202-happy-number/happy-number.cpp
+++ 202-happy-number/happy-number.cpp
@@ -1,3 +1,36 @@
+// class Solution {
+// public:
+//     bool isHappy(int n) {
+//         unordered_set<int> visit;
+//         while(visit.find(n) == visit.end()){
+//             visit.insert(n);
+//         
+        
+//         n = sumOfSquares(n);
+//         if(n==1){
+//              return true;
+//         }
+
+//        else{
+
+//         return false;
+//        }
+        
+//     }
+    
+//     int sumOfSquares(int n){
+//         int output = 0;
+//         while(n>0){
+//             int digits = n%10;
+//             digits = digits*digits;
+//             output += digits;
+//             n=n/10;
+//         }
+//         return output;
+//     }
+// };
+
+
 class Solution {
 public:
     bool isHappy(int n) {
@@ -5,14 +38,15 @@ public:
 
         while(visit.find(n)==visit.end()){
             visit.insert(n);
+        
             n = sumOfsquares(n);
             if(n==1){
                 return true;
             }
-        }
+    }
             return false;
         }
-    
+
 
     int sumOfsquares(int n){
         int output = 0;
