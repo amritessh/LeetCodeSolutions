78e389c2b3b726f0a747c0d7c7e81ef28ea824b5
Amritesh Anand <amriteshanand7@gmail.com>
2025-02-26 15:18:44 -0500

    Time: 0 ms (100.00%) | Memory: 11.4 MB (92.12%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
     void sortColors(vector<int>& arr) {
-        int min_index;
-        for (int i = 0; i < arr.size(); i++) {
-            min_index = i;
-            for (int j = i + 1; j < arr.size(); j++) {
-                if (arr[j] < arr[min_index]) {
-                    min_index = j;
-                }
-            }
-            // Swap current index with minimum element in rest of list
-            int temp = arr[min_index];
-            arr[min_index] = arr[i];
-            arr[i] = temp;
-        }
+        sort(arr.begin(),arr.end());
+        // int min_index;
+        // for (int i = 0; i < arr.size(); i++) {
+        //     min_index = i;
+        //     for (int j = i + 1; j < arr.size(); j++) {
+        //         if (arr[j] < arr[min_index]) {
+        //             min_index = j;
+        //         }
+        //     }
+        //     // Swap current index with minimum element in rest of list
+        //     int temp = arr[min_index];
+        //     arr[min_index] = arr[i];
+        //     arr[i] = temp;
+        // }
     }
 };
