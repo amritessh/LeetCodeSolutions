e55af6da2131fbf7208a098c92c2c0481c77487b
Amritesh Anand <amriteshanand7@gmail.com>
2025-02-26 13:00:29 -0500

    Time: 0 ms (100.00%) | Memory: 11.7 MB (86.18%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.cpp
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -15,7 +15,7 @@ public:
         for(int i = 0 ; i <strs[0].size(); i++){
             char c = strs[0][i];
 
-            for(int j =1 ; j < strs.size(); j++){
+            for(int j = 1 ; j < strs.size(); j++){
                 if(i == strs[j].size() || strs[j][i] != c)
                     return strs[0].substr(0,i);
             }
