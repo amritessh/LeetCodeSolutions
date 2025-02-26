454e65818958ec4006dfb5999296184a377a050c
Amritesh Anand <amriteshanand7@gmail.com>
2025-02-26 11:01:09 -0500

    Time: 0 ms (100.00%) | Memory: 28 MB (97.97%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,20 +1,22 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        unordered_map<int,int> mp;
-        for( int num: nums){
-            if(mp.find(num) == mp.end()){
-                mp[num] = 1;
-            }
-            else{
-                mp[num]+=1;
-            }
-        }
+        sort(nums.begin(),nums.end());
+        return nums[nums.size()/2];
+        // unordered_map<int,int> mp;
+        // for( int num: nums){
+        //     if(mp.find(num) == mp.end()){
+        //         mp[num] = 1;
+        //     }
+        //     else{
+        //         mp[num]+=1;
+        //     }
+        // }
 
-        for(auto entry:mp){
-            if(entry.second>nums.size()/2) return entry.first;
-        }
+        // for(auto entry:mp){
+        //     if(entry.second>nums.size()/2) return entry.first;
+        // }
 
-        return 0;
+        // return 0;
     }
 };
