555ebfd92f0fce46800e594690b2aa100cc4d7dc
Amritesh Anand <amriteshanand7@gmail.com>
2025-02-28 14:24:27 -0500

    Time: 0 ms (100.00%) | Memory: 27.2 MB (80.12%) - LeetSync

--- 344-reverse-string/reverse-string.cpp
+++ 344-reverse-string/reverse-string.cpp
@@ -1,6 +1,18 @@
 class Solution {
 public:
+
+
+    void helper(vector<char>& s, int left, int right){
+        if (left >= right) return;
+        swap(s[left], s[right]);
+        helper(s, left + 1, right - 1);
+    }
     void reverseString(vector<char>& s) {
-        return reverse(s.begin(),s.end());
+        
+        // just use the inbuilt function lol
+        // return reverse(s.begin(),s.end());
+
+        //recursive approach
+        helper(s,0,s.size()-1);
     }
 };
