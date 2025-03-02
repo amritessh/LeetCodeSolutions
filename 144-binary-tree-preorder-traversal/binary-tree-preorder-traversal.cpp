48d2b87432be920bdb105a291d521a123d34d6cd
Amritesh Anand <amriteshanand7@gmail.com>
2025-03-01 19:21:53 -0500

    Time: 0 ms (100.00%) | Memory: 10.8 MB (72.03%) - LeetSync

--- 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
@@ -14,40 +14,40 @@ public:
     
     vector<int> preorderTraversal(TreeNode* root) {
 
-        if(!root) return {};
+        // if(!root) return {};
 
-        stack<TreeNode*> stk;
-        vector<int> res;
-
-        stk.push(root);
-        while(!stk.empty()){
-            root = stk.top();// find the top element in the stack 
-            stk.pop();
-
-            res.push_back(root->val);
-
-            if(root->right){
-                stk.push(root->right);
-            }
-
-            if(root->left){
-                stk.push(root->left);
-            }
-        }
-        return res;
-
-
+        // stack<TreeNode*> stk;
         // vector<int> res;
-    //     preorder(root,res);
-    //     return res;
-    // }
 
-    // void preorder(TreeNode* root, vector<int>& res){
-    //     if(root){
-    //         res.push_back(root->val);
-    //         preorder(root->left,res);
-    //         preorder(root->right,res);
-    //     }
+        // stk.push(root);
+        // while(!stk.empty()){
+        //     root = stk.top();// find the top element in the stack 
+        //     stk.pop();
+
+        //     res.push_back(root->val);
+
+        //     if(root->right){
+        //         stk.push(root->right);
+        //     }
+
+        //     if(root->left){
+        //         stk.push(root->left);
+        //     }
+        // }
+        // return res;
+
+
+        vector<int> res;
+        preorder(root,res);
+        return res;
+    }
+
+    void preorder(TreeNode* root, vector<int>& res){
+        if(root){
+            res.push_back(root->val);
+            preorder(root->left,res);
+            preorder(root->right,res);
+        }
     }
     
 };
