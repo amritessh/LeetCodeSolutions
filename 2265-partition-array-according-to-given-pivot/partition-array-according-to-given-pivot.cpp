4549d5b15543568faed27a09b7f639deb982249c
Amritesh Anand <amriteshanand7@gmail.com>
2025-03-03 17:48:04 -0500

    Time: 0 ms (100.00%) | Memory: 127.7 MB (87.43%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.cpp
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.cpp
@@ -1,27 +1,57 @@
 class Solution {
 public:
     vector<int> pivotArray(vector<int>& nums, int pivot) {
-        list<int> less;
-        list<int> equal;
-        list<int> greater;
+        // list<int> less;
+        // list<int> equal;
+        // list<int> greater;
 
-        for (auto num : nums) {
-            if (num < pivot) {
-                less.push_back(num);
-            } else if (num > pivot) {
-                greater.push_back(num);
-            } else {
-                equal.push_back(num);
+        // for (auto num : nums) {
+        //     if (num < pivot) {
+        //         less.push_back(num);
+        //     } else if (num > pivot) {
+        //         greater.push_back(num);
+        //     } else {
+        //         equal.push_back(num);
+        //     }
+        // }
+
+        // less.insert(less.end(), equal.begin(), equal.end());
+        // less.insert(less.end(), greater.begin(), greater.end());
+
+        // vector<int> ans;
+        // for (auto num : less) {
+        //     ans.push_back(num);
+        // }
+        // return ans;
+
+        int less = 0;
+        int equal = 0;
+        for(int num : nums){
+            if(num<pivot) less++;
+            else if(num == pivot){
+                equal++;
             }
         }
 
-        less.insert(less.end(), equal.begin(), equal.end());
-        less.insert(less.end(), greater.begin(), greater.end());
-
-        vector<int> ans;
-        for (auto num : less) {
-            ans.push_back(num);
+            vector<int> ans(nums.size());
+            int lessI = 0;
+            int equalI = less;
+            int greaterI = less + equal;
+            for(int i = 0 ; i < nums.size(); i++){
+            int num = nums[i];
+            if (num < pivot) {
+                ans[lessI] = num;
+                lessI++;
+            } else if (num > pivot) {
+                ans[greaterI] = num;
+                greaterI++;
+            } else {
+                ans[equalI] = num;
+                equalI++;
+            
+        }
         }
         return ans;
-    }
+        }
+    
 };
