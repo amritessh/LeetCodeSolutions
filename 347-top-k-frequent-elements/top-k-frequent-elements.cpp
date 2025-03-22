38ac4643a019b9eecb7fa1e473cc0e81c3dfa4b4
Amritesh Anand <amriteshanand7@gmail.com>
2025-03-22 11:38:30 -0700

    Time: 1 ms (67.46%) | Memory: 19 MB (18.53%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -8,15 +8,20 @@ public:
             mp[num]++;
         }
 
+        //convert the frequency-number map into array
         vector<pair<int,int>> arr;
         for(const auto&p:mp){
             arr.push_back({p.second,p.first});
         }
 
+        //sort this array into descending order because we want to extract top k elements 
         sort(arr.rbegin(),arr.rend());
 
+        //resulting vector
         vector<int> res;
 
+
+        //just pick the top k elements from the array.
         for(int i = 0; i < k ; i++){
             res.push_back(arr[i].second);
         }
