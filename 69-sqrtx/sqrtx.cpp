c509de2548fcfdbb2b39ea92e7bc1bed2045c0b9
Amritesh Anand <amriteshanand7@gmail.com>
2025-03-23 20:02:07 -0700

    Time: 3 ms (19.19%) | Memory: 8.5 MB (85.13%) - LeetSync

--- 69-sqrtx/sqrtx.cpp
+++ 69-sqrtx/sqrtx.cpp
@@ -1,9 +1,6 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        if(x<2) return x;
-        long left = static_cast<long>(exp(0.5*log(x)));
-        long right = left+1;
-        return right * right > x ? left : right;
+        return (int) sqrt(x);
     }
 };
