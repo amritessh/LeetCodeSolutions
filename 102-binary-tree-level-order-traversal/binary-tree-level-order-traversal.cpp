ea8386ce343592e6087f98c3345e63f4bfdfe201
Amritesh Anand <amriteshanand7@gmail.com>
2025-04-01 13:38:28 -0400

    Time: 1 ms (41.89%) | Memory: 17.8 MB (6.26%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.cpp
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.cpp
@@ -11,29 +11,45 @@
  */
 class Solution {
 public:
+    
+    //recursive solution helper function
+    vector<vector<int>> levels;
+    void helper(TreeNode* node, int level){
+        if(levels.size() == level) levels.push_back(vector<int>());
+        levels[level].push_back(node->val);
+        if(node->left != nullptr) helper(node->left, level+1);
+        if(node->right != nullptr) helper(node->right, level+1);
+    }
+    
     vector<vector<int>> levelOrder(TreeNode* root) {
-               vector<vector<int>> ans;
-        queue<TreeNode*> q;
-        if (root) {
-            q.push(root);
-        }
-        TreeNode* cur;
-        while (!q.empty()) {
-            int size = q.size();
-            ans.push_back(vector<int>());
-            for (int i = 0; i < size; ++i) {		// traverse nodes in the same level
-                cur = q.front();
-                q.pop();
-                ans.back().push_back(cur->val);		// visit the root
-                if (cur->left) {
-                    q.push(cur->left);				// push left child to queue if it is not null
-                }
-                if (cur->right) {
-                    q.push(cur->right);				// push right child to queue if it is not null
-                }
-            }
-        }
-        return ans;
+        //recursion
+        if(root==nullptr) return levels;
+        helper(root,0);
+        return levels;        
+        
+        //iterative
+        // vector<vector<int>> ans;
+        // queue<TreeNode*> q;
+        // if (root) {
+        //     q.push(root);
+        // }
+        // TreeNode* cur;
+        // while (!q.empty()) {
+        //     int size = q.size();
+        //     ans.push_back(vector<int>());
+        //     for (int i = 0; i < size; ++i) {		// traverse nodes in the same level
+        //         cur = q.front();
+        //         q.pop();
+        //         ans.back().push_back(cur->val);		// visit the root
+        //         if (cur->left) {
+        //             q.push(cur->left);				// push left child to queue if it is not null
+        //         }
+        //         if (cur->right) {
+        //             q.push(cur->right);				// push right child to queue if it is not null
+        //         }
+        //     }
+        // }
+        // return ans;
     
     }
 };
