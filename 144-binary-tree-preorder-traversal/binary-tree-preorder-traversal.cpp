b4235cc87e46587a797ae8efce5386e64678f701
Amritesh Anand <amriteshanand7@gmail.com>
2025-04-01 10:55:04 -0400

    Time: 0 ms (100.00%) | Memory: 10.6 MB (99.50%) - LeetSync

--- 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
@@ -13,30 +13,58 @@ class Solution {
 public:
     
     vector<int> preorderTraversal(TreeNode* root) {
-
-        if(!root) return {};
-
-        stack<TreeNode*> stk;
-        vector<int> res;
-
-        stk.push(root);
-        while(!stk.empty()){
-            root = stk.top();// find the top element in the stack 
-            stk.pop();
-
-            res.push_back(root->val);
-
-            if(root->right){
-                stk.push(root->right);
-            }
-
-            if(root->left){
-                stk.push(root->left);
+        //morris traversal        
+        vector<int> output;
+        TreeNode* node = root;
+        while (node != nullptr) {
+            if (node->left == nullptr) {
+                output.push_back(node->val);
+                node = node->right;
+            } else {
+                TreeNode* predecessor = node->left;
+                while (predecessor->right != nullptr &&
+                       predecessor->right != node) {
+                    predecessor = predecessor->right;
+                }
+                if (predecessor->right == nullptr) {
+                    output.push_back(node->val);
+                    predecessor->right = node;
+                    node = node->left;
+                } else {
+                    predecessor->right = nullptr;
+                    node = node->right;
+                }
             }
         }
-        return res;
+        return output;
 
 
+
+
+        //iterative
+        // if(!root) return {};
+
+        // stack<TreeNode*> stk;
+        // vector<int> res;
+
+        // stk.push(root);
+        // while(!stk.empty()){
+        //     root = stk.top();// find the top element in the stack 
+        //     stk.pop();
+
+        //     res.push_back(root->val);
+
+        //     if(root->right){
+        //         stk.push(root->right);
+        //     }
+
+        //     if(root->left){
+        //         stk.push(root->left);
+        //     }
+        // }
+        // return res;
+
+    //recursive
     //     vector<int> res;
     //     preorder(root,res);
     //     return res;
