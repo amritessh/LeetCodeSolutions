1d84ddbe9c5f228db7d8af8d6f78cfb4fe0b3826
Amritesh Anand <amriteshanand7@gmail.com>
2025-04-15 18:58:07 -0400

    Time: 8 ms (20.47%) | Memory: 27.8 MB (12.06%) - LeetSync

---
+++ 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -0,0 +1,77 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+//     int post_idx;
+//     vector<int> postorder;
+//     vector<int> inorder;
+//     unordered_map<int,int> idx_map;
+
+// public:
+
+//     TreeNode* helper(int in_left, int in_right){
+//         //if there are no elements to construct subtrees
+//         if(in_left>in_right) return NULL;
+
+//         //pick up post_idx element as a root
+//         int root_val = postorder[post_idx];
+//         TreeNode* root = new TreeNode(root_val);
+
+//         //
+//     }
+
+
+// class Solution {
+    int post_idx;
+    vector<int> postorder;
+    vector<int> inorder;
+    unordered_map<int, int> idx_map;
+
+public:
+    TreeNode* helper(int in_left, int in_right) {
+        // if there are no elements to construct subtrees
+        if (in_left > in_right) return NULL;
+        // pick up post_idx element as a root
+        int root_val = postorder[post_idx];
+        TreeNode* root = new TreeNode(root_val);
+        // root splits inorder list
+        // into left and right subtrees
+        int index = idx_map[root_val];
+        // recursion
+        post_idx--;
+        // build the right subtree
+        root->right = helper(index + 1, in_right);
+        // build the left subtree
+        root->left = helper(in_left, index - 1);
+        return root;
+    }
+    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
+        this->postorder = postorder;
+        this->inorder = inorder;
+        // start from the last postorder element
+        post_idx = postorder.size() - 1;
+        // build a hashmap value -> its index
+        int idx = 0;
+        for (int val : inorder) idx_map[val] = idx++;
+        return helper(0, inorder.size() - 1);
+    }
+};
+
+        // this->postorder = postorder;
+        // this->inorder = inorder;
+
+        // post_idx = postorder.size()-1;
+
+        // int idx = 0;
+        // for(int val : inorder) idx_map[val] = idx++;
+        // return helper(0,indorder.size()-1);
+//     }
+// };
