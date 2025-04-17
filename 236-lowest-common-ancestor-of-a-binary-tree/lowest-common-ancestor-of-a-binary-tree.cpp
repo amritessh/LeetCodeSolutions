bdab4ee33a9232c6b3dd8c8518bd09e15079fa78
Amritesh Anand <amriteshanand7@gmail.com>
2025-04-16 20:25:28 -0400

    Time: 13 ms (46.44%) | Memory: 17.2 MB (88.39%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
@@ -18,5 +18,12 @@ public:
         if(right == NULL) return left;
         
         else return root;
+
+        // if(!root || !p || !q){ return NULL;}
+
+        // if(p->val < root->val && q->val < root->val){ return lowestCommonAncestor(root->left,p,q);}
+        // else if(p->val > root->val && q->val > root->val) {return lowestCommonAncestor(root->right,p,q);}
+
+        //  else{ return root;}
     }
 };
