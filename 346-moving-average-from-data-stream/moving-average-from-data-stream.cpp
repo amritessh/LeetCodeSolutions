e18076d6a2b9b937d50d31fa23c8cd6a6169c110
Amritesh Anand <amriteshanand7@gmail.com>
2025-05-16 12:46:38 -0400

    Time: 0 ms (100.00%) | Memory: 20.8 MB (77.06%) - LeetSync

--- 346-moving-average-from-data-stream/moving-average-from-data-stream.cpp
+++ 346-moving-average-from-data-stream/moving-average-from-data-stream.cpp
@@ -1,21 +1,44 @@
 class MovingAverage {
 
-private: 
-    int size, windowSum = 0, count = 0;
-    deque<int> queue;
 
-public:
-    MovingAverage(int size){this->size=size;}
+    private:
+        int size;
+        int head = 0 ;
+        int windowSum = 0;
+        int count = 0;
+        vector<int> queue;
+
+    public:
+        MovingAverage(int size){
+            this->size = size;
+            queue = vector<int> (size);
+        }
 
     double next(int val){
         count++;
-        queue.push_back(val);
-        int tail = count>size ? queue.front():0;
-        if(count>size) queue.pop_front();
-
-        windowSum = windowSum - tail + val;
-        return static_cast<double>(windowSum)/min(size,count);
+        int tail = (head+1)%size;
+        windowSum = windowSum - queue[tail] + val;
+        head = (head+1)%size;
+        queue[head]=val;
+        return windowSum*1.0/min(size,count);
     }
+
+// private: 
+//     int size, windowSum = 0, count = 0;
+//     deque<int> queue;
+
+// public:
+//     MovingAverage(int size){this->size=size;}
+
+//     double next(int val){
+//         count++;
+//         queue.push_back(val);
+//         int tail = count>size ? queue.front():0;
+//         if(count>size) queue.pop_front();
+
+//         windowSum = windowSum - tail + val;
+//         return static_cast<double>(windowSum)/min(size,count);
+//     }
 
 
 
