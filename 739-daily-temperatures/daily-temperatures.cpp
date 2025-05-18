b7e0f2aad5266e44820e6e0a10bafbada2bbe423
Amritesh Anand <amriteshanand7@gmail.com>
2025-05-18 13:55:05 -0400

    Time: 28 ms (36.85%) | Memory: 107.2 MB (54.59%) - LeetSync

---
+++ 739-daily-temperatures/daily-temperatures.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        int n = temperatures.size();
+        vector<int> answer(n, 0); // Initialize result with 0s
+        stack<int> stk; // Stores indices of days with unresolved warmer days
+        
+        for (int currDay = 0; currDay < n; ++currDay) {
+            int currentTemp = temperatures[currDay];
+            
+            // Pop indices from the stack while current temp is higher
+            while (!stk.empty() && temperatures[stk.top()] < currentTemp) {
+                int prevDay = stk.top();
+                stk.pop();
+                answer[prevDay] = currDay - prevDay; // Update waiting days
+            }
+            
+            stk.push(currDay); // Push current day to stack
+        }
+        
+        return answer;
+    }
+};
