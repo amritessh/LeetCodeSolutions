0944622338b9c03693ec4f9f8bfb5804421933ec
Amritesh Anand <amriteshanand7@gmail.com>
2025-05-20 19:42:19 -0400

    Time: 0 ms (100.00%) | Memory: 21.6 MB (99.67%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -3,7 +3,7 @@ public:
     int missingNumber(vector<int>& nums) {
         int sum = 0;
         int n = nums.size();
-        for(int i = 0 ; i < nums.size(); i++) sum+=nums[i];
+        for(int i = 0 ; i < n; i++) sum+=nums[i];
         return (n*(n+1)/2) - sum; 
     }
 };
