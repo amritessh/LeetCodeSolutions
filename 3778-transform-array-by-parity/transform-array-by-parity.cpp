e4c36b146c78aa956d3553e5ea0fcdc17a51a7b2
Amritesh Anand <amriteshanand7@gmail.com>
2025-06-17 15:21:41 -0400

    Time: 0 ms (100.00%) | Memory: 28.3 MB (62.30%) - LeetSync

---
+++ 3778-transform-array-by-parity/transform-array-by-parity.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> transformArray(vector<int>& nums) {
+        // sort(nums.begin(),nums.end());
+        vector<int> res;
+        for(int i = 0 ; i < nums.size() ; i++){
+            if(nums[i]%2==0) res.push_back(0);
+            else res.push_back(1);
+        }
+        sort(res.begin(),res.end());
+        return res;
+    }
+};
