c68b35137e626e6a124bfa7e1118c21978c5d856
Amritesh Anand <amriteshanand7@gmail.com>
2025-06-28 15:16:42 -0400

    Time: 0 ms (100.00%) | Memory: 16.1 MB (92.72%) - LeetSync

---
+++ 261-graph-valid-tree/graph-valid-tree.cpp
@@ -0,0 +1,51 @@
+#include <vector>
+using namespace std;
+
+class UnionFind {
+private:
+    vector<int> parent;
+    vector<int> size;
+public:
+    UnionFind(int n) {
+        parent.resize(n);
+        size.resize(n, 1);
+        for (int i = 0; i < n; i++) parent[i] = i;
+    }
+
+    int find(int A) {
+        int root = A;
+        while (root != parent[root]) root = parent[root];
+        while (A != root) {
+            int oldRoot = parent[A];
+            parent[A] = root;
+            A = oldRoot;
+        }
+        return root;
+    }
+
+    bool unite(int A, int B) {
+        int rootA = find(A);
+        int rootB = find(B);
+        if (rootA == rootB) return false;
+        if (size[rootA] < size[rootB]) {
+            parent[rootA] = rootB;
+            size[rootB] += size[rootA];
+        } else {
+            parent[rootB] = rootA;
+            size[rootA] += size[rootB];
+        }
+        return true;
+    }
+};
+
+class Solution {
+public:
+    bool validTree(int n, vector<vector<int>>& edges) {
+        if (edges.size() != n - 1) return false;
+        UnionFind uf(n);
+        for (auto& edge : edges) {
+            if (!uf.unite(edge[0], edge[1])) return false;
+        }
+        return true;
+    }
+};
