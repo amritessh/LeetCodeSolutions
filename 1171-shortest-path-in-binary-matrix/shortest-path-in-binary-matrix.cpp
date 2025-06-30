6554e5d68564a269274594b3e9429e0fcadd9843
Amritesh Anand <amriteshanand7@gmail.com>
2025-06-30 15:15:03 -0400

    Time: 27 ms (41.88%) | Memory: 25.1 MB (62.96%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
@@ -10,27 +10,41 @@ public:
 
         vector<pair<int,int>> directions = {{0,0},{1,0},{0,1},{1,1},{0,-1},{-1,0},{-1,-1},{1,-1},{-1,1}};
 
+        //initialising a visit vector matrix
         vector<vector<bool>> visit(N,vector<bool>(N,false));
 
+        //initialising a queue of r,c,path_length to traverse in BFS manner
         queue<tuple<int,int,int>> q;
+        //initialise the queue with first cell, 1 as path length
         q.push({0,0,1});
+        //mark the first cell as visited
         visit[0][0] = true;
 
+        //while loop to process the input in BFS manner
         while(!q.empty()){
+            //process the queue in FIFO manner, pop the element at the front
             auto[r,c,length] = q.front();
             q.pop();
 
+            //check if row and colum have reached the destination, then return the length
             if(r == N-1 && c == N-1) return length;
-
+            
+            //otherwise keep traversing in all the directions
             for(auto [dr,dc]: directions){
+                //initialise the neighboring row and neighboring column with the current row and coulumn with the directions
                 int nr = r+dr, nc = c+dc;
-                if(nr>=0 && nc>=0 && nr<N && nc<N && grid[nr][nc]==0 && !visit[nr][nc]){
+
+                //checking iff the neighboring row and column are traversable which is within the bounds or the cell is not blocked or the cell is not in the visited vector
+                if(nr>=0 && nc>=0 && nr<N && nc<N && grid[nr][nc]==0 && !visit[nr][nc]){    
+                    //push the cell to the queue to process it 
                     q.push({nr,nc,length+1});
+                    //mark the cell as visited 
                     visit[nr][nc] = true;
 
                 }
             }
         }
+        //return -1 if no path found
     return -1;
     }
 };
