b039f7b28c1ff14a7cc3219dec02f221864570a1
Amritesh Anand <amriteshanand7@gmail.com>
2025-07-09 13:51:37 -0400

    Time: 3 ms (70.85%) | Memory: 102 MB (84.39%) - LeetSync

--- 463-island-perimeter/island-perimeter.cpp
+++ 463-island-perimeter/island-perimeter.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
     int islandPerimeter(vector<vector<int>>& grid) {
-        int p=0;
-        for(int i = 0 ; i < grid.size(); i++){
-            for(int j = 0 ; j < grid[i].size(); j++){
-                if(grid[i][j]){
-                    if(i-1 < 0 || !grid[i-1][j]) p++;
-                    if(i+1 >= grid.size() || !grid[i+1][j]) p++;
-                    if(j-1 < 0 || !grid[i][j-1]) p++;
-                    if(j+1 >= grid[i].size() || !grid[i][j+1]) p++; 
+        int ans =0;
+        for(int i =0;i<grid.size();i++){
+            for(int j = 0;j<grid[0].size();j++){
+                if(grid[i][j] == 1){ 
+                    ans+=4; 
+if(i+1<grid.size() && grid[i+1][j]==1)ans--;//if down neighbour is there
+if(i-1>=0 && grid[i-1][j]==1)ans--; // if upward neighbour is there
+if(j-1>=0 && grid[i][j-1]==1)ans--;// if left neighbour is there
+if(j+1<grid[0].size() && grid[i][j+1]==1)ans--;//if right neighbour is there
                 }
             }
         }
-        return p;
+        return ans;
     }
 };
