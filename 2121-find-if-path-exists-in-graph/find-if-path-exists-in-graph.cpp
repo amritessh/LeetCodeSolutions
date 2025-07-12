3c5be432acdf66cc64a08e039a2fba852f1718d5
Amritesh Anand <amriteshanand7@gmail.com>
2025-07-12 16:49:49 -0400

    Time: 529 ms (23.88%) | Memory: 345.8 MB (25.02%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.cpp
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
-    bool dfs(unordered_map<int,vector<int>>&graph, vector<bool>&seen,int currNode, int destination){
-        if(currNode == destination){
+    bool dfs(unordered_map<int, vector<int>>& graph, vector<bool>& seen, int currNode, int destination) {
+        if (currNode == destination) {
             return true;
         }
         seen[currNode] = true;
-        for(int nextNode: graph[currNode]){
-            if(!seen[nextNode]){
-                if(dfs(graph,seen,nextNode,destination)){
+        for (int nextNode : graph[currNode]) {
+            if (!seen[nextNode]) {  // Only call dfs if not seen
+                if (dfs(graph, seen, nextNode, destination)) {
                     return true;
                 }
             }
@@ -15,17 +15,15 @@ public:
         return false;
     }
     
-    
-    
-    
     bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
-        unordered_map<int,vector<int>> graph;
-        for(const auto& edge:edges){
+        unordered_map<int, vector<int>> graph;
+        for (const auto& edge : edges) {
             int a = edge[0], b = edge[1];
             graph[a].push_back(b);
             graph[b].push_back(a);
         }
-        vector<bool> seen(n,false);
-        return dfs(graph,seen,source,destination);
+
+        vector<bool> seen(n, false);
+        return dfs(graph, seen, source, destination);
     }
 };
