83ec40e070613a33be1c5b3e941a63767455fb63
Amritesh Anand <amriteshanand7@gmail.com>
2025-07-12 10:53:20 -0400

    Time: 35 ms (65.32%) | Memory: 56.7 MB (62.16%) - LeetSync

--- 1308-smallest-string-with-swaps/smallest-string-with-swaps.cpp
+++ 1308-smallest-string-with-swaps/smallest-string-with-swaps.cpp
@@ -1,68 +1,64 @@
-class UnionFind {
+
+class UnionFind{
 private:
-    vector<int> root;
-    vector<int> rank;
+    vector<int> parent,rank;
+
+
 public:
-    // Initialize the array root and rank
-    // Each vertex is representative of itself with rank 1
-    UnionFind(int sz) : root(sz), rank(sz) {
-        for (int i = 0; i < sz; i++) {
-            root[i] = i;
-            rank[i] = 1;
-        }
+UnionFind(int size){
+    parent.resize(size);
+    rank.resize(size,0);
+    for(int i = 0; i < size; i++){
+        parent[i] = i;
     }
-    
-    // Get the root of a vertex
-    int find(int x) {
-        if (x == root[x]) {
-            return x;
-        }
-        return root[x] = find(root[x]);
+}
+
+    int find(int x ){
+        if(parent[x]!=x) parent[x] = find(parent[x]);
+        return parent[x];
     }
 
-    // Perform the union of two components
-    void unionSet(int x, int y) {
+    void union_set(int x, int y){
         int rootX = find(x);
         int rootY = find(y);
-        if (rootX != rootY) {
-            if (rank[rootX] >= rank[rootY]) {
-                root[rootY] = rootX;
-                rank[rootX] += rank[rootY];
-            } else {
-                root[rootX] = rootY;
-                rank[rootY] += rank[rootX];
+
+        if(rootX!=rootY){
+            if(rank[rootX]<rank[rootY]) parent[rootY] = rootX;
+            else if(rank[rootX]>rank[rootY]) parent[rootX] = rootY;
+            else{
+                parent[rootX] = rootY;
+                rank[rootX]++;
             }
         }
     }
+    
 };
+
+/*
+1. build a graph using the given pairs, 
+2. find the connected components in the graph, 
+3. sort the characters in each connected component in ascending order
+4. build the smallest string.
+*/
 
 class Solution {
 public:
     string smallestStringWithSwaps(string s, vector<vector<int>>& pairs) {
-        UnionFind uf(s.size());
-        
-        // Iterate over the edges
-        for (vector<int> edge : pairs) {
-            int source = edge[0];
-            int destination = edge[1];
-            
-            // Perform the union of end points
-            uf.unionSet(source, destination);
+        UnionFind dsu(s.size());
+
+        for(auto& pair:pairs){
+            int u = pair[0],v=pair[1];
+            dsu.union_set(u,v);
         }
-        
-        
-        unordered_map<int, vector<int>> rootToComponent;
-        // Group the vertices that are in the same component
-        for (int vertex = 0; vertex < s.size(); vertex++) {
-            int root = uf.find(vertex);
-            // Add the vertices corresponding to the component root
+
+        unordered_map<int,vector<int>> rootToComponent;
+        for(int vertex = 0; vertex < s.size(); vertex++){
+            int root = dsu.find(vertex);
             rootToComponent[root].push_back(vertex);
         }
-        
-        // String to store the answer
+
         string smallestString(s.length(), ' ');
-        // Iterate over each component
-        for (auto component : rootToComponent) {
+            for (auto component : rootToComponent) {
             vector<int> indices = component.second;
             
             // Sort the characters in the group
