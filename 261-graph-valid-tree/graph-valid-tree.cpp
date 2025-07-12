707f664e6d63d2f764d223e66c3327e51e62db4b
Amritesh Anand <amriteshanand7@gmail.com>
2025-07-12 10:06:23 -0400

    Time: 0 ms (100.00%) | Memory: 16.4 MB (71.84%) - LeetSync

--- 261-graph-valid-tree/graph-valid-tree.cpp
+++ 261-graph-valid-tree/graph-valid-tree.cpp
@@ -1,49 +1,54 @@
+class UnionFind{
+private: 
+    vector<int> parent,rank;
 
-class UnionFind {
-private:
-    vector<int> parent;
-    vector<int> size;
+
 public:
-    UnionFind(int n) {
-        parent.resize(n);
-        size.resize(n, 1);
-        for (int i = 0; i < n; i++) parent[i] = i;
+UnionFind(int size){
+    parent.resize(size);
+    rank.resize(size,0);
+    for(int i = 0 ; i < size; i++){
+        parent[i] = i;
     }
+}
 
-    int find(int A) {
-        int root = A;
-        while (root != parent[root]) root = parent[root];
-        while (A != root) {
-            int oldRoot = parent[A];
-            parent[A] = root;
-            A = oldRoot;
-        }
-        return root;
-    }
 
-    bool unite(int A, int B) {
-        int rootA = find(A);
-        int rootB = find(B);
-        if (rootA == rootB) return false;
-        if (size[rootA] < size[rootB]) {
-            parent[rootA] = rootB;
-            size[rootB] += size[rootA];
-        } else {
-            parent[rootB] = rootA;
-            size[rootA] += size[rootB];
+int find(int x){
+    if(parent[x]!=x) parent[x]=find(parent[x]);
+    return parent[x];
+}
+
+
+void union_set(int x, int y){
+    int rootX = find(x), rootY = find(y);
+
+    if(rootX!=rootY){
+        if(rank[rootX]<rank[rootY]) parent[rootY] = rootX;
+        else if(rank[rootX]>rank[rootY]) parent[rootX] = rootY;
+        else{
+            parent[rootX] = rootY;
+            rank[rootX]++;
         }
-        return true;
     }
+}
+
+
 };
 
 class Solution {
 public:
     bool validTree(int n, vector<vector<int>>& edges) {
-        if (edges.size() != n - 1) return false;
-        UnionFind uf(n);
-        for (auto& edge : edges) {
-            if (!uf.unite(edge[0], edge[1])) return false;
+        int connectedComponents = n;
+        UnionFind dsu(n);
+        if(edges.size()!=n-1) return false;
+        for(auto& edge:edges){
+            int u = edge[0], v = edge[1];
+
+            if(dsu.find(u)!=dsu.find(v)){
+                connectedComponents--;
+                dsu.union_set(u,v);
+            }
         }
-        return true;
+        return connectedComponents==1;
     }
 };
