4a01922718b98b36962fb440bd241048098fc909
Amritesh Anand <amriteshanand7@gmail.com>
2026-03-14 23:07:20 -0400

    Time: 0 ms (100.00%) | Memory: 17.1 MB (21.08%) - LeetSync

---
+++ 1704-special-positions-in-a-binary-matrix/special-positions-in-a-binary-matrix.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int numSpecial(vector<vector<int>>& mat) {
+        vector<int> rows(mat.size()), cols(mat[0].size());
+        for(int i = 0 ; i < mat.size() ; i++){
+            for(int j = 0 ; j < mat[0].size(); j++){
+                if(mat[i][j]) rows[i]++, cols[j]++;
+            }
+        }
+
+        int res = 0;
+        for(int i = 0 ; i < mat.size() ; i++){
+            for(int j = 0 ; j < mat[0].size(); j++){
+                if(mat[i][j] && rows[i]==1 && cols[j]==1) res++;
+            }
+        }
+        return res;
+    }
+};
