a1e6e680952dd4988ec6c4dbf3acd85683d79dc5
Amritesh Anand <amriteshanand7@gmail.com>
2026-07-17 13:50:48 -0400

    Time: 0 ms (100.00%) | Memory: 8.5 MB (87.45%) - LeetSync

--- 69-sqrtx/sqrtx.cpp
+++ 69-sqrtx/sqrtx.cpp
@@ -1,6 +1,30 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        return (int) sqrt(x);
+        //always square root of x is going to be less than x/2 thats why set up boundary like that
+        int left = 2, right = x/2; 
+        long num;
+        int pivot;
+
+        if (x<2) return x;
+        while(left<=right){
+            pivot = left + (right - left)/2;
+            //take a guess that pivot is the square root of x
+            num = (long)pivot * pivot ;
+
+
+            if( num < x){
+                left = pivot + 1;
+            }
+
+            else if(num > x){
+                right = pivot - 1;
+            }
+
+            else return pivot;
+
+        }
+
+        return right;
     }
 };
